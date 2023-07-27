5e7251e50f0a5f835dc30679c5937e71540c8a01
teammate22 <timvarsey@gmail.com>
2023-07-27 21:12:08 +0300

    complete 2.1.3

--- src/week 2.ts
+++ src/week 2.ts
@@ -1,6 +1,27 @@
 import './style.css'
 
 // задание 1
+let arr = [5, 12, 76, 23, 4, 1, 54, 87, 33, 41]
+
+function showArr(arr: any){
+  console.log(arr)
+}
+showArr(arr)
+
+function showEven(arr: any){
+  let evenArr = []
+  for(let i of arr){
+    if(i%2 == 0) evenArr.push(i)
+  }
+  console.log(`Чётные числа ${evenArr}`)
+}
+showEven(arr)
+
+function sumEl(arr: any){
+  let sum = arr.reduce((sum: number, el: number) => sum + el, 0)
+  console.log(`Сумма элементов ${sum}`)
+}
+sumEl(arr)
 
 // задание 2
 
